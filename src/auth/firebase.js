c42f77d7ea23e816ac0694f0be886fce2616a057
Ali <ali.ozgur@example.com>
2023-02-25 01:29:30 +0300

    added bootstrap and firabase config

---
+++ src/auth/firebase.js
@@ -0,0 +1,24 @@
+
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+import { getAuth } from "firebase/auth";
+
+// firebase bizim için verdiği  konfigürasyon dosyasını import ediyoruz.
+const firebaseConfig = {
+  apiKey: "AIzaSyAME7TpxwrhqnLa8Jspr8aSocx1WTzRCF8",
+  authDomain: "picture-gallery-2023.firebaseapp.com",
+  projectId: "picture-gallery-2023",
+  storageBucket: "picture-gallery-2023.appspot.com",
+  messagingSenderId: "685111525078",
+  appId: "1:685111525078:web:d7085e91c421c1b16b9377",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// const auth = getAuth(app);
+
+// fire base ile kullanıcı oluşturmak için kullanılan yöntem
