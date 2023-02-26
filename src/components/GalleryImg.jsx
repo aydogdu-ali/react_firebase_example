5a828f65fee7ddad8d461c014bf6084e2c744759
Ali <ali.ozgur@example.com>
2023-02-27 02:42:43 +0300

    added GallerImg component and edit Detail pages

---
+++ src/components/GalleryImg.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+
+const GalleryImg = ({data}) => {
+  const { largeImageURL, user,  id } = data;
+  const navigate = useNavigate();
+
+  // console.log(data)
+
+  const detailHandle = () => {
+   
+      navigate(`/detail/${id}`);
+   
+  };
+
+  return (
+    <div className="container card-container">
+      <div class="card ">
+        <img src={largeImageURL} className="card-img fluid" alt= {user}/>
+        <div class="card-body">
+          <h5 class="card-title">Name: {user}</h5>
+
+          <button  className="btn btn-warning" onClick={detailHandle}>
+            Detail
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default GalleryImg
+
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,49 +1,88 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import loading from "../assets/loading.gif"
 
-const Detail = ({data}) => {
-const { largeImageURL, user, likes, downloads, comments, id } = data;
+const Detail = () => {
+  const [image, setImage] = useState({});
+  const [loading, setLoading] = useState(true);
+  const { id } = useParams(); 
+ 
 
-  // console.log(data)
 
-  // const detailHandle = () => {
-  //   if (giris) {
-  //     navigate(`/detail/${id}`);
-  //   } else {
-  //     toast.error("Login to see details");
-  //     navigate("/login");
-  //   }
-  // };
+  const api_key = "31725179-e9547203f59a4095ebc0c6c08";
+
+  const url = `https://pixabay.com/api/?key=${api_key}&image_type=photo&lang=en&id=${id}`;
+
+const getFetchData = () => {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      setImage(data.hits);
+      setLoading(false);
+    })
+    .catch((error) => console.log(error));
+};
+
+useEffect(() => {
+  getFetchData();
+}, []);
+
+
 
   return (
-    <div>
-      <div className="cardd container">
-        <img src={largeImageURL} className="img-fluid" alt="img" />
-        <div className="card-bodyy">
-          <p className="card-titlee mt-4">
-            Username: <span className="username">{user}</span>
-          </p>
-          <div className="icons d-flex justify-content-evenly align-items-center">
-            <div className="icon">
-              <i className="like-icon fas fa-solid fa-heart"></i>
-              <span> {likes}</span>
+    <div className="   mt-4">
+      {loading && (
+        <div>
+          <img src={loading} alt="loading" />
+        </div>
+      )}
+      <>
+        <Link to="/gallery">
+          <button className="btn btn-warning mb-2">Back to search</button>
+        </Link>
+        <br />
+        <div className="image-url">
+          <img src={image[0]?.largeImageURL} alt="detail" />
+        </div>
+        <div className="mt-2 p-3 bg-dark w-75 mx-auto rounded rounded-5">
+          <div className="row w-50 mx-auto">
+            <div className="col-sm-12 col-md-6">
+              <h6 className="detail-title text-danger fw-bolder">Tags</h6>
+              <p className="text-success">{image[0]?.tags}</p>
             </div>
-            <div className="icon">
-              <i className="download-icon fas fa-thin fa-cloud-arrow-down"></i>
-              <span> {downloads}</span>
-            </div>
-            <div className="icon">
-              <i className="comments-icon fas fa-sharp fa-regular fa-comment"></i>
-              <span> {comments} </span>
+            <div className="col-sm-12 col-md-6">
+              <h6 className="detail-title text-danger fw-bolder">Size</h6>
+              <p className="text-success">
+                {image[0]?.webformatHeight} x {image[0]?.webformatWidth}
+              </p>
             </div>
           </div>
-          {/* <button onClick={detailHandle} className="btn btn-outline btn-detail">
-            Details
-          </button> */}
+
+          <div className="row w-50 mx-auto align-items-center-justify-content-evenly text-center ">
+            <div className="col-sm-12 col-md-6">
+              <h6 className="detail-title text-danger fw-bolder">Like</h6>
+              <p className="text-success"> {image[0]?.likes} </p>
+            </div>
+            <div className="col-sm-12 col-md-6">
+              <h6 className="detail-title text-danger fw-bolder">Comment</h6>
+              <p className="text-success"> {image[0]?.comments} </p>
+            </div>
+            <div className="col-sm-12 col-md-6">
+              <h6 className="detail-title text-danger fw-bolder">View</h6>
+              <p className="text-success"> {image[0]?.views} </p>
+            </div>
+            <div className="col-sm-12 col-md-6">
+              <h6 className="detail-title text-danger fw-bolder">Download</h6>
+              <p className="text-success"> {image[0]?.downloads} </p>
+            </div>
+          </div>
         </div>
-      </div>
-    
+      </>
+      <Link to="/gallery">
+        <button className="btn btn-warning m-2">Back to search</button>
+      </Link>
     </div>
   );
 };
 
-export default Detail
+export default Detail;
